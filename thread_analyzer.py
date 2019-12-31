b1679d8b97ccba1aff3e39d5153859487ac0489a
rolinj <>
2019-12-31 11:25:36 +0800

    Rename variables

--- thread_analyzer.py
+++ thread_analyzer.py
@@ -1,12 +1,12 @@
 import requests
 
-URL = "https://slack.com/api/conversations.history?token=<APP_TOKEN>&channel=<CHANNEL_ID>&oldest=<UNIX_TIMESTAMP>&latest=<UNIX_TIMESTAMP>"
+API_URL = "https://slack.com/api/conversations.history?token=<APP_TOKEN>&channel=<CHANNEL_ID>&oldest=<UNIX_TIMESTAMP>&latest=<UNIX_TIMESTAMP>"
 
 #Dictionary of members to be checked.
-members = {"<USER_SLACK_ID>":"<USERNAME>"}
-counter = 0
+team_members = {"<USER_SLACK_ID>":"<USERNAME>"}
+response_counter = 0
 
-r = requests.get(url=URL)
+r = requests.get(url=API_URL)
 data = r.json()
 
 #Loop on every message on the channel.
@@ -14,12 +14,13 @@ for message in data["messages"]:
     
     #Check if the message is a thread or not.
     if "thread_ts" in message.keys():
-        for details in message["replies"]:
+        for thread_response in message["replies"]:
 
             #Check if listed member is among the thread responders.
-            if details["user"] in members.keys():
-                counter += 1
-                print(f"{members[details['user']]} replied on thread --> {message['text']}")
+            if thread_response["user"] in team_members.keys():
+                response_counter += 1
+                
+                print(f"{team_members[thread_response['user']]} replied on thread --> {message['text']}")
 
                 #Add break to exit the loop. This is to ignore multiple replies on a single thread.
                 #Also ignores replies of multiple members. 
@@ -27,4 +28,4 @@ for message in data["messages"]:
 
 print("#########")
 print("#########")
-print(f"Total thread responses by members: {counter}")
+print(f"Total thread responses by members: {response_counter}")

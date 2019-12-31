480fb209bd9a4932a583e412f0d780f24f1d3258
rolinj <>
2019-12-31 10:34:34 +0800

    Add code comments

--- thread_analyzer.py
+++ thread_analyzer.py
@@ -6,13 +6,16 @@ members = {"<USER_SLACK_ID>":"<USERNAME>"}
 counter = 0
 
 r = requests.get(url=URL)
-
 data = r.json()
 
+#Loop on every message on the channel.
 for message in data["messages"]:
+    
+    #Check if the message is a thread or not.
     if "thread_ts" in message.keys():
-
         for details in message["replies"]:
+
+            #Check if listed member is among the thread responders.
             if details["user"] in members.keys():
                 counter += 1
                 print(f"{members[details['user']]} replied on thread --> {message['text']}")
